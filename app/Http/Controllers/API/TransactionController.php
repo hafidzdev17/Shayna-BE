2654658b56e5247cacde75af40a5d528032b9ee7
mhafet17 <do.crazy192@gmail.com>
2020-05-30 08:49:53 +0700

    create API CheckoutController

---
+++ app/Http/Controllers/API/TransactionController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Transaction;
+
+class TransactionController extends Controller
+{
+    public function get(Request $request, $id)
+    {
+        $product = Transaction::with(['details.product'])->find($id);
+
+        if ($product) {
+            return ResponseFormatter::success($product, 'Data transaksi berhasil diambil');
+        } else {
+            return ResponseFormatter::error(null, 'Data Transaksi tidak ada', 404);
+        }
+    }
+}
